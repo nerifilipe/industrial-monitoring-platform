8037842adb7fc6818c63bddd098e503615b342a7
nerifilipe <152918669+nerifilipe@users.noreply.github.com>
2026-05-12 15:37:04 +0100

    fix dependencies

---
+++ backend/src/main/java/com/cv/industrialmonitoring/IndustrialMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.cv.industrialmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustrialMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustrialMonitoringApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cv/industrialmonitoring/IndustrialMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cv.industrialmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustrialMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

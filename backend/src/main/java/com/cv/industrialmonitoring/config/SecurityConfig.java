f48f6ac139dfcf36ea3eec54537f518b07f421cf
nerifilipe <filipenerigg@gmail.com>
2026-05-13 21:57:58 +0100

    Update SecurityConfig.java

--- backend/src/main/java/com/cv/industrialmonitoring/config/SecurityConfig.java
+++ backend/src/main/java/com/cv/industrialmonitoring/config/SecurityConfig.java
@@ -50,9 +50,9 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of(
+                configuration.setAllowedOriginPatterns(List.of(
                 "http://localhost:5173",
-                "https://industrial-monitoring-platform.vercel.app"
+                "https://*.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of(

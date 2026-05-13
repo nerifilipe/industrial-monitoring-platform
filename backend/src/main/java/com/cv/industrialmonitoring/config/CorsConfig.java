d6fda470252a9025955f56677c9b378818cadb65
nerifilipe <152918669+nerifilipe@users.noreply.github.com>
2026-05-13 21:32:39 +0100

    cors update

--- backend/src/main/java/com/cv/industrialmonitoring/config/CorsConfig.java
+++ backend/src/main/java/com/cv/industrialmonitoring/config/CorsConfig.java
@@ -17,8 +17,12 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("*");
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://industrial-monitoring-platform-fn.vercel.app/"
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
             }
         };
     }

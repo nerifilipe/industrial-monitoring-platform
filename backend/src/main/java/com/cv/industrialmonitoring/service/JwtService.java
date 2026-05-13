1bb825d618d8988d3490f10e0afeadee3f79f363
nerifilipe <152918669+nerifilipe@users.noreply.github.com>
2026-05-13 13:21:34 +0100

    JWT completed

--- backend/src/main/java/com/cv/industrialmonitoring/service/JwtService.java
+++ backend/src/main/java/com/cv/industrialmonitoring/service/JwtService.java
@@ -1,5 +1,6 @@
 package com.cv.industrialmonitoring.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
@@ -15,12 +16,29 @@ public class JwtService {
     private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 24;
 
     public String generateToken(String username) {
-
         return Jwts.builder()
                 .subject(username)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
                 .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        return extractClaims(token)
+                .getExpiration()
+                .after(new Date());
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 }
